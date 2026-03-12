128d9df6d3094437af8cc3213720784a26d278cb
sureshsangineedi <suresh.sangineedi@outlook.com>
2026-03-12 11:27:38 +0530

    adding update

--- src/data/products.js
+++ src/data/products.js
@@ -18,6 +18,34 @@ name:"Leather Jacket",
 price:7000,
 image:"https://images.unsplash.com/photo-1520975916090-3105956dac38"
 }
+,
+{
+id:4,
+name:"Leather Jacket",
+price:4000,
+image:"https://images.unsplash.com/photo-1520975916090-3105956dac38"
+}
+,
+{
+id:5,
+name:"Leather Jacket",
+price:10000,
+image:"https://images.unsplash.com/photo-1592158249887-ac6ae7921691?q=80&w=687&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+}
+,
+{
+id:6,
+name:"Leather Jacket",
+price:9000,
+image:"https://images.unsplash.com/photo-1602370463198-086436840055?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+},
+{
+id:7,
+name:"Leather Jacket",
+price:9000,
+image:"https://images.unsplash.com/photo-1602370463198-086436840055?q=80&w=627&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+}
+
 ]
 
 export default products
